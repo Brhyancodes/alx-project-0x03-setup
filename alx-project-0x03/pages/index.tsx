5d5adca534cf1a9ee85dd91577e6906abeb1d7dd
Brian Wakhale <brianweke1998@gmail.com>
2025-01-12 17:09:22 +0300

    Modfied pages directory

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,18 +1,13 @@
-import Button from "@/components/common/Button";
-import { useRouter } from "next/router";
-import { PageRouteProps } from "@/interface";
-
-interface PageRouteProps {
-  pageRoute: string;
-}
+import Button from '@/components/common/Button'
+import { PageRouteProps } from '@/interface'
+import { useRouter } from 'next/router'
 
 export default function Home() {
-  const router = useRouter();
-
-  // Imeperative routing with useRouter
+  // ['const router = useRouter();']
+  const router = useRouter()
   const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
-    router.push(pageRoute, undefined, { shallow: false });
-  };
+    router.push(pageRoute, undefined, { shallow: false })
+  }
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
@@ -28,21 +23,21 @@ export default function Home() {
       {/* Navigation Options */}
       <div className="flex gap-6">
         <Button
-          action={() => routeToNextPage({ pageRoute: "/generate-text-ai" })}
+          action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })}
           buttonLabel="Generate Text"
           buttonBackgroundColor="blue"
         />
         <Button
-          action={() => routeToNextPage({ pageRoute: "/text-to-image" })}
+          action={() => routeToNextPage({ pageRoute: '/text-to-image' })}
           buttonLabel="Text to Image"
           buttonBackgroundColor="green"
         />
         <Button
-          action={() => routeToNextPage({ pageRoute: "/counter-app" })}
+          action={() => routeToNextPage({ pageRoute: '/counter-app' })}
           buttonLabel="Contact us"
           buttonBackgroundColor="orange"
         />
       </div>
     </div>
-  );
+  )
 }
